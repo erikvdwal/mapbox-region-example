b80a02603c5379985481cdf18275a3bf300f1ace
Erik van der Wal <erikvdwal@gmail.com>
2017-07-31 21:26:38 +0200

    Initial Commit

---
+++ MapboxRegionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapboxRegionExample
+//
+//  Created by Erik van der Wal on 31/07/2017.
+//  Copyright © 2017 Erik van der Wal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
